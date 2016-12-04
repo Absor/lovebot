4d1428649d2ce904d6cea9a7dcb48000780832f5
Heikki Haapala <hhaapala@gmail.com>
2016-12-04 17:46:16 +0200

    Add record and replay functions

--- test/test_replaying.js
+++ test/test_replaying.js
@@ -61,4 +61,80 @@ describe('Replaying game', () => {
     game.joinGameAsPlayer('tester2');
     game.startGameAsPlayer('tester1');
   });
+
+  it('is possible using record and replay', (done) => {
+    // "Bot" play
+    game.on('event', (event) => {
+      if (event.type !== 'privateStatus') return;
+      if (event.isCurrentPlayer === false) return;
+
+      const cardToPlay = event.cardsInHand.find(card => card.canPlay);
+
+      const cardName = cardToPlay.name;
+      const cardTarget = cardToPlay.validTargetPlayers.length !== 0 ?
+                         cardToPlay.validTargetPlayers[0] : null;
+
+      const choice = cardName === 'Guard' ? 'Baron' : null;
+
+      game.playCardAsPlayer(event.for[0], cardName, cardTarget, choice);
+    });
+
+    game.on('event', (event) => {
+      if (event.type !== 'gameEnd') return;
+
+      // Check that we can get the record
+      const record = game.getRecord();
+      expect(record).to.not.be.empty;
+
+      // When replaying, the other game should have the same record
+      otherGame.replayRecord(record);
+      expect(otherGame.getRecord()).to.deep.equal(record);
+
+      done();
+    });
+
+    // Start!
+    game.joinGameAsPlayer('tester1');
+    game.joinGameAsPlayer('tester2');
+    game.startGameAsPlayer('tester1');
+  });
+
+  it('is done by using game inputs as the record to replay', (done) => {
+    const gameInputs = [];
+
+    game.on('input', (event) => {
+      gameInputs.push(event);
+    });
+
+    // "Bot" play
+    game.on('event', (event) => {
+      if (event.type !== 'privateStatus') return;
+      if (event.isCurrentPlayer === false) return;
+
+      const cardToPlay = event.cardsInHand.find(card => card.canPlay);
+
+      const cardName = cardToPlay.name;
+      const cardTarget = cardToPlay.validTargetPlayers.length !== 0 ?
+                         cardToPlay.validTargetPlayers[0] : null;
+
+      const choice = cardName === 'Guard' ? 'Baron' : null;
+
+      game.playCardAsPlayer(event.for[0], cardName, cardTarget, choice);
+    });
+
+    game.on('event', (event) => {
+      if (event.type !== 'gameEnd') return;
+
+      // Record equals all inputs
+      const record = game.getRecord();
+      expect(record).to.deep.equal(gameInputs);
+
+      done();
+    });
+
+    // Start!
+    game.joinGameAsPlayer('tester1');
+    game.joinGameAsPlayer('tester2');
+    game.startGameAsPlayer('tester1');
+  });
 });
